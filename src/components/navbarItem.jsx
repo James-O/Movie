673689f6a315604f836a15239d3a674534bd897f
Ogbonna James <Ogbonnasunday42@gmail.com>
2024-05-28 19:13:20 +0100

    keep looking for solution

--- src/components/navbarItem.jsx
+++ src/components/navbarItem.jsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link'
-import React from 'react'
+import { Suspense,React } from 'react'
 import { useSearchParams } from 'next/navigation'
 
 export default function NavbarItem({title,param}) {
@@ -8,10 +8,10 @@ export default function NavbarItem({title,param}) {
     const genre = searchParams.get('genre');
 
   return (
-    <div>
+    <Suspense>
         <Link
         className={`hover:text-amber-600 font-semibold ${genre===param?'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg':''}`}
         href={`/?genre=${param}`}>{title}</Link>
-    </div>
+    </Suspense>
   )
 }
