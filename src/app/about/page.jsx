092a050c0853ccf6e72d8650b6215043ff7364ba
Ogbonna James <Ogbonnasunday42@gmail.com>
2024-05-28 19:12:08 +0100

    keep looking for solution

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,6 +7,7 @@ export default function About() {
             <p>Welcome to our movie database website! We are a team of passionate movie enthusiasts who have come together to create a one-stop destination for all your movie-related needs.</p>
             <p>Our website is designed to provide you with comprehensive database of movies  from all round the world, along with the latest news, reviews, and trailers. our movie database is constantly updated with new releases, ensuring that you have access to the latest and greagest in the world of cinema. You can research for movies by title, director, actor, genre, or release date, making it easy to find the perfect movie for any occasion.</p>
             <p>In addition to our extensive movie database, we also offer a platform for a movie lovers to connect and share there thoughts on the latest releases. Our community section include a forum where you can discuss your favourite fils with like-minded individuals and read reviews and ratings from other users. We also have a section dedicated for movie news and trailers, keeping you updated with the latest hapennings inthe world of cinema. Thank you for visiting our website and we hope you enjoyed your time browsing through our movie database. If you have any feedback or suggestions, please feel free to contact us. We are always looking for a way to improve and enhance the user experience on our website. Happy browsing!</p>
+            <p>Thank you specially</p>
     </div>
   )
 }
