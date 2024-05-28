54d4695ecaf5cf13a512578b024f995efd543db1
Ogbonna James <Ogbonnasunday42@gmail.com>
2024-05-28 18:35:38 +0100

    added next-theme to resolve production error

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import Results from '@/components/results';
 import React from 'react'
 
+const API_KEY = process.env.API_KEY;
 export default async function Home({searchParams}) {
   const genre = searchParams.genre || 'fetchTreanding';
   const res = await fetch(`
   https://api.themoviedb.org/3${
-    genre==='fetchTopRated'?`/movie/top_rated`:`/trending/movie/week`}?api_key=7772b80fed6bbdbc413f1213dabadc8b&laguage=en-Us&page=1
+    genre==='fetchTopRated'?`/movie/top_rated`:`/trending/movie/week`}?api_key=${API_KEY}&laguage=en-Us&page=1
   `);
   const data = await res.json();
   if(!res.ok){
