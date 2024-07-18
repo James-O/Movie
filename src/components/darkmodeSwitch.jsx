34b0a6b4dadcf633306f789c149e955b61345214
Ogbonna James <Ogbonnasunday42@gmail.com>
2024-07-18 17:36:12 +0100

    made corrections on dark mode

--- src/components/darkmodeSwitch.jsx
+++ src/components/darkmodeSwitch.jsx
@@ -1,13 +1,24 @@
 'use client';
 
-import React from 'react'
 import {MdLightMode, MdDarkMode} from 'react-icons/md'
 import {useTheme} from 'next-themes'
+import { useEffect, useState } from 'react';
 
 export default function DarkmodeSwitch() {
-    const {theme, setTheme, systhemTheme}=useTheme()
-    const currentTheme = theme ==='system'?systhemTheme:theme
-  return (
-    <div>{currentTheme==='dark' ? <MdLightMode onClick={()=>{setTheme('light')}} className='text-xl cursor-pointer hover:text-amber-500'/>:<MdDarkMode onClick={()=>{setTheme('dark')}} className='text-xl cursor-pointer hover:text-amber-500'/>}</div>
+    const {theme, setTheme, systhemTheme}=useTheme();
+    const [mounted, setMounted] = useState(false)
+    const currentTheme = theme === 'system' ? systhemTheme:theme
+  
+    useEffect(()=>{
+        setMounted(true)
+      },[]);
+
+    return (
+    <div>
+      {mounted && (currentTheme==='dark' ? (
+      <MdLightMode onClick={()=>{setTheme('light')}} className='text-xl cursor-pointer hover:text-amber-500'/>
+      ):(
+      <MdDarkMode onClick={()=>{setTheme('dark')}} className='text-xl cursor-pointer hover:text-amber-500'/>))}
+    </div>
   )
 }
