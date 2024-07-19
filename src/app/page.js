15f99dd919672859beec5388eb5fb9287b219808
Ogbonna James <Ogbonnasunday42@gmail.com>
2024-07-19 01:38:22 +0100

    error corrected

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import {Suspense} from 'react'
 
 const API_KEY = process.env.API_KEY;
 export default async function Home({searchParams}) {
-  const genre = searchParams.genre || 'fetchTreanding';
+  const genre = searchParams.genre || 'fetchTrending';
+  //const genre = 'fetchTrending';
   const res = await fetch(`
   https://api.themoviedb.org/3${
     genre==='fetchTopRated'?`/movie/top_rated`:`/trending/movie/week`}?api_key=${API_KEY}&laguage=en-Us&page=1
