5a70033fa4bc95c36e40532077be51b3533d2728
Ogbonna James <Ogbonnasunday42@gmail.com>
2024-09-06 23:12:19 +0100

    fetched data from tmdb

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,34 @@
-'use client'
-import React, { useEffect, useState } from 'react'
+// 'use client'
+// import React, { useEffect, useState } from 'react'
 
-//const API_KEY = process.env.API_KEY;
-export default function Home() {
-    const [movieList,setMovieList] = useState([])
-    const getMovie = ()=>{
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=7772b80fed6bbdbc413f1213dabadc8b&laguage=en-Us&page=1")
-        .then(res =>res.json())
-        .then(json =>setMovieList(json.results));
+import Results from "@/components/results";
+
+const API_KEY = process.env.API_KEY;
+export default async function Home({searchParams}) {
+    // const [movieList,setMovieList] = useState([])
+    const genre = searchParams.genre || 'fetchTrending';
+
+    const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`);
+    const data = await res.json();
+    if(!res.ok){
+        throw new Error('failed to fetch data')
     }
+    const results = data.results;
+    //console.log(results);
+    // const getMovie = ()=>{
+    //     fetch("https://api.themoviedb.org/3/discover/movie?api_key=7772b80fed6bbdbc413f1213dabadc8b&laguage=en-Us&page=1")
+    //     .then(res =>res.json())
+    //     .then(json =>setMovieList(json.results));
+    // }
    
-    useEffect(()=>{
-        getMovie()
-    },[]);
+    // useEffect(()=>{
+    //     getMovie()
+    // },[]);
 
   return (
     <div>
-        {movieList &&
+        <Results results={results}/>
+        {/* {movieList &&
             (movieList.map((movie)=>(
                 <div key={movie.id}>
                     <img style={{width:'300px',height:'250px'}} 
@@ -24,7 +36,7 @@ export default function Home() {
                     <p>{movie.title}</p>
                 </div>
             )))
-        }
+        } */}
     </div>
   )
 }
