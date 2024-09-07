0d2e9daf2b16ab86c6fc1b8a76e989dc544b1225
Ogbonna James <Ogbonnasunday42@gmail.com>
2024-09-07 15:04:00 +0100

    add movie page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
-// 'use client'
-// import React, { useEffect, useState } from 'react'
-
 
 import Results from "@/components/results";
 
 const API_KEY = process.env.API_KEY;
 export default async function Home({searchParams}) {
-    // const [movieList,setMovieList] = useState([])
     const genre = searchParams.genre || 'fetchTrending';
 
     const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`);
@@ -15,30 +11,12 @@ export default async function Home({searchParams}) {
         throw new Error('failed to fetch data')
     }
     const results = data.results;
-    //console.log(results);
-    // const getMovie = ()=>{
-    //     fetch("https://api.themoviedb.org/3/discover/movie?api_key=7772b80fed6bbdbc413f1213dabadc8b&laguage=en-Us&page=1")
-    //     .then(res =>res.json())
-    //     .then(json =>setMovieList(json.results));
-    // }
-   
-    // useEffect(()=>{
-    //     getMovie()
-    // },[]);
+    //console.log(results)
 
   return (
     
         <div>
-        <Results results={results}/>
-        {/* {movieList &&
-            (movieList.map((movie)=>(
-                <div key={movie.id}>
-                    <img style={{width:'300px',height:'250px'}} 
-                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
-                    <p>{movie.title}</p>
-                </div>
-            )))
-        } */}
+            <Results results={results}/>
         </div>
     
   )
